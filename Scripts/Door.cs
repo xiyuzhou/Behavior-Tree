28a1969edb0b545d7cc65572daeae3280ee7e4ab
xiyuzhou <xyz2333@outlook.com>
2023-04-07 10:32:47 -0700

    Project setup

---
+++ Scripts/Door.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool isClosed = false;
+    public bool isLocked = false;
+
+    Vector3 closedRotation = new Vector3(0, 0, 0);
+    Vector3 openRotation = new Vector3(0, -135, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isClosed)
+        {
+            transform.eulerAngles = closedRotation;
+        }
+        else
+        {
+            transform.eulerAngles = openRotation;
+        }
+    }
+    public void OnClose(bool stat)
+    {
+        if (stat)
+        {
+            Close();
+        }
+        else
+        {
+            Open();
+        }
+    }
+    public void OnLock(bool stat)
+    {
+        isLocked = !stat;
+        Locked();
+    }
+    public bool Open()
+    {
+        if (isClosed && !isLocked)
+        {
+            //Debug.Log("door is now open");
+            isClosed = false;
+            transform.eulerAngles = openRotation;
+            return true;
+        }
+
+        //Debug.Log("door was either locked or already open");
+        return false;
+    }
+
+    public bool Close()
+    {
+        if (!isClosed)
+        {
+            //Debug.Log("door is now closed");
+            transform.eulerAngles = closedRotation;
+            isClosed = true;
+        }
+        return true;
+    }
+    public void Locked()
+    {
+        if (isLocked == false)
+        {
+            isLocked = true;
+        }
+        else
+        {
+            isLocked = false;
+        }
+    }
+}
